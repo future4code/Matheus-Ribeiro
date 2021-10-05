5baf0a93ae7afe99cf62b9e0e2108a8808e2c9e4
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2021-10-05 17:08:18 -0300

    exercício 07 resolvido

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -51,7 +51,7 @@ function retornaExpressoesBooleanas() {
   const booleano2 = false
   const booleano3 = !booleano2
   const booleano4 = !booleano3
-  
+
   const respostas = [
     booleano1 && booleano2 && !booleano4,
     (booleano1 && booleano2) || !booleano3,
@@ -65,7 +65,12 @@ function retornaExpressoesBooleanas() {
 
 // EXERCÍCIO 07
 function retornaNNumerosPares(n) {
+  let numerosPares = []
+  for (let i = 0; i < n; i++) {
+    numerosPares.push(i * 2)
+  }
 
+  return numerosPares
 }
 
 // EXERCÍCIO 08
