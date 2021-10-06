ee540ccb92e13f57ca2c988bb8ad519cb513d9bd
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2021-10-05 21:08:29 -0300

    exercício 10 resolvido

--- quinzena3/revisao-js/exercicios.js
+++ quinzena3/revisao-js/exercicios.js
@@ -102,14 +102,14 @@ function comparaDoisNumeros(num1, num2) {
     objeto.maiorNumero = num2
     objeto.maiorDivisivelPorMenor = verificarDivisao(num2, num1)
     objeto.diferenca = num2 - num1
-    
+
     return objeto
 
   } else {
     objeto.maiorNumero = num1
     objeto.maiorDivisivelPorMenor = verificarDivisao(num1, num2)
     objeto.diferenca = num1 - num2
-    
+
 
     return objeto
   }
@@ -117,80 +117,94 @@ function comparaDoisNumeros(num1, num2) {
 
 // EXERCÍCIO 10
 function segundoMaiorEMenor(array) {
-
+  let arraySegundoMaiorMenor = []
+  let aux = 0
+  for (let contador = 1; contador < array.length; contador++) {
+    for (let i = 0; i < array.length - 1; i++) {
+      if (array[i] > array[i + 1]) {
+        aux = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = aux
+      }
+    }
+  }
+  arraySegundoMaiorMenor[0] = array[array.length - 2]
+  arraySegundoMaiorMenor[1] = array[1]
+  
+  return arraySegundoMaiorMenor
 }
 
-// EXERCÍCIO 11
-function ordenaArray(array) {
+  // EXERCÍCIO 11
+  function ordenaArray(array) {
+    
+  }
 
-}
+  // EXERCÍCIO 12
+  function filmeFavorito() {
 
-// EXERCÍCIO 12
-function filmeFavorito() {
+  }
 
-}
+  // EXERCÍCIO 13
+  function imprimeChamada() {
+    // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
+  }
 
-// EXERCÍCIO 13
-function imprimeChamada() {
-  // "Venha assistir ao filme NOME_DO_FILME, de ANO, dirigido por DIRECAO e estrelado por ELENCO."
-}
+  // EXERCÍCIO 14
+  function criaRetangulo(lado1, lado2) {
 
-// EXERCÍCIO 14
-function criaRetangulo(lado1, lado2) {
+  }
 
-}
+  // EXERCÍCIO 15
+  function anonimizaPessoa(pessoa) {
 
-// EXERCÍCIO 15
-function anonimizaPessoa(pessoa) {
+  }
 
-}
+  // EXERCÍCIO 16A
+  function maioresDe18(arrayDePessoas) {
 
-// EXERCÍCIO 16A
-function maioresDe18(arrayDePessoas) {
+  }
 
-}
+  // EXERCÍCIO 16B
+  function menoresDe18(arrayDePessoas) {
 
-// EXERCÍCIO 16B
-function menoresDe18(arrayDePessoas) {
+  }
 
-}
+  // EXERCÍCIO 17A
+  function multiplicaArrayPor2(array) {
 
-// EXERCÍCIO 17A
-function multiplicaArrayPor2(array) {
+  }
 
-}
+  // EXERCÍCIO 17B
+  function multiplicaArrayPor2S(array) {
 
-// EXERCÍCIO 17B
-function multiplicaArrayPor2S(array) {
+  }
 
-}
+  // EXERCÍCIO 17C
+  function verificaParidade(array) {
 
-// EXERCÍCIO 17C
-function verificaParidade(array) {
+  }
 
-}
+  // EXERCÍCIO 18A
+  function retornaPessoasAutorizadas(pessoas) {
 
-// EXERCÍCIO 18A
-function retornaPessoasAutorizadas(pessoas) {
+  }
 
-}
+  // EXERCÍCIO 18B
+  function retornaPessoasNaoAutorizadas(pessoas) {
 
-// EXERCÍCIO 18B
-function retornaPessoasNaoAutorizadas(pessoas) {
+  }
 
-}
+  // EXERCÍCIO 19A
+  function ordenaPorNome(consultasNome) {
 
-// EXERCÍCIO 19A
-function ordenaPorNome(consultasNome) {
+  }
 
-}
+  // EXERCÍCIO 19B
+  function ordenaPorData(consultasData) {
 
-// EXERCÍCIO 19B
-function ordenaPorData(consultasData) {
+  }
 
-}
+  // EXERCÍCIO 20
+  function calculaSaldo(contas) {
 
-// EXERCÍCIO 20
-function calculaSaldo(contas) {
-
-}
+  }
