ac9cc8786bcbb184e0974c0bcf96e5718ed54343
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2021-11-19 16:28:12 -0300

    fiz a implementação de posts através do map

--- modulo3/estados-insta4/insta4/src/App.js
+++ modulo3/estados-insta4/insta4/src/App.js
@@ -10,24 +10,52 @@ const MainContainer = styled.div`
 `;
 
 class App extends React.Component {
+  state = {
+    posts: [
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario: 'https://picsum.photos/id/12/50/50',
+        fotoPost: 'https://picsum.photos/id/30/200/150',
+      },
+      {
+        nomeUsuario: 'bananinha',
+        fotoUsuario: 'https://picsum.photos/id/128/50/50',
+        fotoPost: 'https://picsum.photos/id/200/200/150',
+      },
+      {
+        nomeUsuario: 'labenu',
+        fotoUsuario: 'https://picsum.photos/id/239/50/50',
+        fotoPost: 'https://picsum.photos/id/10/200/150',
+      },
+    ],
+  };
+
   render() {
+    const listaDePosts = this.state.posts.map((post) => {
+      return (
+        <Post
+          nomeUsuario={post.nomeUsuario}
+          fotoUsuario={post.fotoUsuario}
+          fotoPost={post.fotoPost}
+        />
+      );
+    });
+
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/id/12/50/50'}
-          fotoPost={'https://picsum.photos/id/30/200/150'}
-        />
-        <Post
-          nomeUsuario={'bananinha'}
-          fotoUsuario={'https://picsum.photos/id/128/50/50'}
-          fotoPost={'https://picsum.photos/id/20/200/150'}
-        />
-        <Post
-          nomeUsuario={'labenu'}
-          fotoUsuario={'https://picsum.photos/id/239/50/50'}
-          fotoPost={'https://picsum.photos/id/10/200/150'}
-        />
+        {/* Fiz desse jeito para testar se dava certo, e deu :D */}
+
+        {/* {this.state.posts.map((post) => {
+          return (
+            <Post
+              nomeUsuario={post.nomeUsuario}
+              fotoUsuario={post.fotoUsuario}
+              fotoPost={post.fotoPost}
+            />
+          );
+        })} */}
+
+        {listaDePosts}
       </MainContainer>
     );
   }
