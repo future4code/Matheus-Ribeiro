156fe1f1ca48c42da3c5f10a3f81b7d13323553b
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2021-11-23 19:21:01 -0300

    arrumei a etapa 3, que estava sem a pergunta do select

--- modulo3/rend-condicional-form/src/pages/Etapa3.js
+++ modulo3/rend-condicional-form/src/pages/Etapa3.js
@@ -7,7 +7,8 @@ export class Etapa3 extends Component {
     return (
       <div>
         <h2>ETAPA 3 - INFORMAÇÕES GERAIS DE ENSINO</h2>
-        <InputText pergunta="7. Por que você não terminou um curso de graduação?"/>
+        <InputText pergunta="5. Por que você não terminou um curso de graduação?"/>
+        <InputText pergunta="6. Você fez algum curso complementar?"/>
         <InputSelect selecao={["Curso técnico", "Curso de inglês", "Não fiz curso complementar"]}/>
       </div>
     )
