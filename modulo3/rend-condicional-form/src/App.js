2d3c9e8b72422def0156e194b52df2da4558098e
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2021-11-23 19:04:00 -0300

    fiz a etapa 2 e seus componentes

--- modulo3/rend-condicional-form/src/App.js
+++ modulo3/rend-condicional-form/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Etapa1 from './pages/Etapa1';
+import Etapa2 from './pages/Etapa2';
 
 class App extends React.Component {
   render() {
     return <div className="App">
       <Etapa1/>
+      <Etapa2/>
     </div>;
   }
 }
