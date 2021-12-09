079a23cb035cec6f7a690135b32eae2315b50dc7
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2021-12-09 20:33:24 -0300

    fiz a parte de pegar a lista de usuários

--- modulo3/apis-labenusers/src/components/ListaUsuarios.js
+++ modulo3/apis-labenusers/src/components/ListaUsuarios.js
@@ -1,10 +1,36 @@
+import axios from 'axios';
 import React from 'react';
 
 export default class ListaUsuarios extends React.Component {
   state = {
     listaUsuarios: [],
   };
+
+  componentDidMount() {
+    this.pegarUsuarios();
+  }
+
+  pegarUsuarios = () => {
+    const URL = 'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users';
+
+    const headers = {
+      headers: {
+        Authorization: 'matheus-ribeiro-joy',
+      },
+    };
+
+    axios
+      .get(URL, headers)
+      .then((res) => this.setState({ listaUsuarios: res.data }))
+      .catch((err) => console.log(err.data));
+  };
   render() {
-    return <div></div>;
+    return (
+      <div>
+        {this.state.listaUsuarios.map((usuario) => (
+          <p key={usuario.id}>{usuario.name}</p>
+        ))}
+      </div>
+    );
   }
 }
