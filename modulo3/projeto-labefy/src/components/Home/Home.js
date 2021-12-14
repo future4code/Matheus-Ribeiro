2905cb06e3781f33f7f11f62cd4711270b1b8577
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2021-12-14 18:13:18 -0300

    fiz a funcao de deletar playlist

--- modulo3/projeto-labefy/src/components/Home/Home.js
+++ modulo3/projeto-labefy/src/components/Home/Home.js
@@ -35,15 +35,33 @@ export default class Home extends Component {
       })
   }
 
+  deletarPlaylists = (id) => {
+    if (window.confirm("Tem certeza que deseja deletar essa playlist?")) {
+      axios.delete(`${URL}/${id}`, headers)
+        .then((res) => {
+          alert("Playlist deletada com sucesso!")
+          this.pegarPlaylists()
+        })
+        .catch((err) => {
+          console.log(err.message)
+        })
+    }
+  }
+
   renderizarPagina = () => {
     switch (this.state.pagina) {
       case "playlists":
         return (
-          <Playlists playlists={this.state.playlists} pegarPlaylists={this.pegarPlaylists} />
+          <Playlists playlists={this.state.playlists}
+            pegarPlaylists={this.pegarPlaylists}
+            deletarPlaylist={this.deletarPlaylists}
+          />
         )
       case "criarPlaylist":
         return (
-          <CriarPlaylist pegarPlaylists={this.pegarPlaylists} />
+          <CriarPlaylist
+            pegarPlaylists={this.pegarPlaylists}
+          />
         )
       default: return
     }
@@ -62,7 +80,10 @@ export default class Home extends Component {
       <>
         <Header />
         <ContainerGrid>
-          <MenuLateral paginaCriarPlaylist={this.paginaCriarPlaylist} paginaPlaylists={this.paginaPlaylists} />
+          <MenuLateral
+            paginaCriarPlaylist={this.paginaCriarPlaylist}
+            paginaPlaylists={this.paginaPlaylists}
+          />
           {this.renderizarPagina()}
         </ContainerGrid>
       </>
--- modulo3/projeto-labefy/src/components/Playlists/Playlists.js
+++ modulo3/projeto-labefy/src/components/Playlists/Playlists.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
 
 export default class Playlists extends Component {
-  
+
   componentDidMount() {
     this.props.pegarPlaylists()
   }
 
   render() {
     const listaPlaylists = this.props.playlists.map((playlist) => {
-      return <p>{playlist.name}</p>
+      return <p>{playlist.name} <span onClick={() => this.props.deletarPlaylist(playlist.id)}>X</span></p>
     })
     return (
       <div>
-       {listaPlaylists}
+        {listaPlaylists}
       </div>
     )
   }
