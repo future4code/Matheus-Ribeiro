2ec9d9adba65ab38b6e91a22492fedc2024f9ff8
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2022-04-05 19:25:58 -0300

    Exercícios 1 a 8 resolvidos

---
+++ modulo6/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,26 @@
+export type Product = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const products: Product[] = [
+    {
+        id: "123-ase2-3123",
+        name: "Notebook",
+        price: 3999.99
+    },
+    {
+        id: "212-fer2-3311",
+        name: "Playstation 5",
+        price: 4989.99
+    },
+    {
+        id: "673-rjt2-6565",
+        name: "Geladeira",
+        price: 2899.99
+    }
+]
+
+
+
---
+++ modulo6/fluxo-de-dados-backend/src/errors.ts
@@ -0,0 +1,17 @@
+export const Errors: { [key: string]: { status: number; message: string } } = {
+  INVALID_PARAMETERS: { status: 422, message: "O preço deve ser maior que 0" },
+  PRODUCT_NOT_FOUND: { status: 404, message: "Produto não encontrado." },
+  MISSING_PARAMETERS: {
+    status: 422,
+    message: "Alguma informação está faltando, verifique os parâmentros.",
+  },
+  INVALID_PARAMETERS_TYPES: {
+    status: 422,
+    message: `Verifique os parâmetros, o "name" dever ser do tipo "string" e o "price" deve ser do tipo "number"`,
+  },
+  INVALID_PARAMETERS_TYPE: {
+    status: 422,
+    message: `Verifique o parâmetro, o "price" deve ser do tipo "number"`,
+  },
+  SOMETHING_WENT_WRONG: { status: 500, message: "Algo deu errado." },
+}
